4db108ff8144a23a02db04aea4e514f0ce7b7a7c
Prakit Junkhum <prakit340@hotmail.com>
2023-10-08 10:00:33 +0700

    commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"633230010@webmail.npru.ac.th\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
